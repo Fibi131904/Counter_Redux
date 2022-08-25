117289230de6c066ee9fa2bbf82a1e8a592136c9
Kseniya Smirnova <fanni@bk.ru>
2022-08-25 12:03:10 +0300

    css counter

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,14 +1,16 @@
+import { Button } from '@material-ui/core';
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { incrementCountAC, resetCountAC } from '../../bll/counterReduser';
 import { AppStateType } from '../../bll/store';
+import style from './Counter.module.css'
 
 
 
 export const Counter = () => {
     const dispatch = useDispatch()
     const value = useSelector<AppStateType, number>(state => state.counter.value);
-   // const startValue = useSelector<AppStateType, number>(state => state.counter.startValue);
+    // const startValue = useSelector<AppStateType, number>(state => state.counter.startValue);
 
 
     const onIncrement = () => {
@@ -17,15 +19,15 @@ export const Counter = () => {
     const onReset = () => {
         dispatch(resetCountAC())
     }
-  
+
     return (
-        <div>
-            <div>{value}</div>
-            <div >
-                <button onClick={onIncrement} >
-                    Inc </button>
-                <button onClick={onReset}>
-                    Reset </button>
+        <div className={style.container}>
+            <span className={style.display}>{value}</span>
+            <div className={style.btn} >
+                <Button onClick={onIncrement} variant="contained" color='primary' size='small' >
+                    Inc </Button>
+                <Button onClick={onReset} variant="contained" color='primary' size='small'>
+                    Reset </Button>
             </div >
         </div >
     )
